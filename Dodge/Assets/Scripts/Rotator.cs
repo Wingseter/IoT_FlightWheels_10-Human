58b8fddc5e670d100f227b1597ebc8ddf669fd05
YongHoon Kwon <wingseter@gmail.com>
2019-10-24 20:48:23 +0900

    add Filt TO Test and Calabration

---
+++ Dodge/Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+    public float rotationSpeed = 60f;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.JoystickButton1))
+        {
+            rotationSpeed = rotationSpeed * -1;
+        }
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+    }
+}
